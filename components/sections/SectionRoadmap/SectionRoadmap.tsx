2266792d817b733d965070b40ea7a5e203bb7c24
danilo-89 <danilo89abcd@gmail.com>
2023-07-17 21:29:08 +0200

    component added

---
+++ components/sections/SectionRoadmap/SectionRoadmap.tsx
@@ -0,0 +1,14 @@
+import Title from '@/components/Title'
+
+const SectionRoadmap = () => {
+    return (
+        <section>
+            <Title title="Roadmap">
+                Lorem Ipsum is simply dummy text of the printy. Minted 21 of
+                1000.
+            </Title>
+        </section>
+    )
+}
+
+export default SectionRoadmap
