7ba3d0ae7797dcd034b058281b560a4c079b8df6
danilo-89 <danilo89abcd@gmail.com>
2023-07-17 21:26:14 +0200

    refactor

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,19 @@
-import Hero from '@/components/Hero';
-import SectionMint from '@/components/sections/SectionMint';
+'use client'
+
+import Hero from '@/components/sections/SectionHero'
+import SectionMint from '@/components/sections/SectionMint'
+import SectionRoadmap from '@/components/sections/SectionRoadmap/SectionRoadmap'
+import useScrollIndicator from './hooks/useScrollIndindicator'
+import Nav from '@/components/Nav'
 
 export default function Home() {
-	return (
-		<main className='max-w-full'>
-			<Hero />
-			<SectionMint />
-		</main>
-	);
+    return (
+        <main className="max-w-full">
+            <Nav />
+
+            <Hero />
+            <SectionMint />
+            <SectionRoadmap />
+        </main>
+    )
 }
