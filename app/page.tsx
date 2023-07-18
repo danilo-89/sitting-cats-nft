ab73181d4af1fffb567d882c90c5fdb25843114d
danilo-89 <danilo89abcd@gmail.com>
2023-07-18 18:23:34 +0200

    added wagmi config and code cleanup

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,34 @@
 'use client'
 
-import Hero from '@/components/sections/SectionHero'
+import { WagmiConfig, createConfig, configureChains, mainnet } from 'wagmi'
+import { publicProvider } from 'wagmi/providers/public'
+
+// Components
+import Nav from '@/components/Nav'
+import SectionHero from '@/components/sections/SectionHero'
 import SectionMint from '@/components/sections/SectionMint'
 import SectionRoadmap from '@/components/sections/SectionRoadmap/SectionRoadmap'
-import useScrollIndicator from './hooks/useScrollIndindicator'
-import Nav from '@/components/Nav'
+
+const { chains, publicClient, webSocketPublicClient } = configureChains(
+    [mainnet],
+    [publicProvider()]
+)
+
+const config = createConfig({
+    autoConnect: true,
+    publicClient,
+    webSocketPublicClient,
+})
 
 export default function Home() {
     return (
-        <main className="max-w-full">
-            <Nav />
-
-            <Hero />
-            <SectionMint />
-            <SectionRoadmap />
-        </main>
+        <WagmiConfig config={config}>
+            <main className="max-w-full overflow-x-hidden">
+                <Nav />
+                <SectionHero />
+                <SectionMint />
+                <SectionRoadmap />
+            </main>
+        </WagmiConfig>
     )
 }
