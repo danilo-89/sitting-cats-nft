64e651937b0084b4b322547b29907e0498d1aee0
danilo-89 <danilo89abcd@gmail.com>
2023-08-08 15:16:32 +0200

    component added

---
+++ components/common/Modal/Modal.tsx
@@ -0,0 +1,33 @@
+import { ReactNode } from 'react'
+import * as Dialog from '@radix-ui/react-dialog'
+
+interface IProps {
+    children: ReactNode
+    preventOverlayClose?: boolean
+    setIsOpen: any
+}
+
+const Modal = ({ children, preventOverlayClose, setIsOpen }: IProps) => {
+    return (
+        <Dialog.Root
+            onOpenChange={setIsOpen}
+            modal
+            open
+        >
+            <Dialog.Portal>
+                <Dialog.Overlay className="fixed inset-0 z-10 flex items-center justify-center bg-[rgba(0,0,0,0.75)]">
+                    <Dialog.Content
+                        className="mx-auto rounded bg-white"
+                        onInteractOutside={(e) => {
+                            if (preventOverlayClose) e.preventDefault()
+                        }}
+                    >
+                        {children}
+                    </Dialog.Content>
+                </Dialog.Overlay>
+            </Dialog.Portal>
+        </Dialog.Root>
+    )
+}
+
+export default Modal
