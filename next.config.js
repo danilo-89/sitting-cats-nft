422e3f78b77745644ef865d999b42c8c8098f16a
danilo-89 <danilo89abcd@gmail.com>
2023-08-08 15:12:27 +0200

    added image host

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'i.postimg.cc',
+                port: '',
+                // pathname: '/account123/**',
+            },
+        ],
+    },
+}
 
 module.exports = nextConfig
