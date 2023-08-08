187d90e0d19dddfc2562cf2de8c0cd58163a16e6
danilo-89 <danilo89abcd@gmail.com>
2023-08-08 15:20:03 +0200

    component refactoring, code cleanup WIP

--- components/sections/SectionMint/SectionMint.tsx
+++ components/sections/SectionMint/SectionMint.tsx
@@ -1,23 +1,124 @@
+import { useEffect, useRef, useState } from 'react'
+import Image from 'next/image'
+import {
+    useAccount,
+    useContractWrite,
+    usePrepareContractWrite,
+    useContractRead,
+    useWaitForTransaction,
+} from 'wagmi'
+import { fromHex, parseEther, parseUnits } from 'viem'
+import debounce from 'lodash.debounce'
+
+// Components
 import AngledContentStripe from '@/components/shared/AngledContentStripe/AngledContentStripe'
 import Button from '@/components/common/Button/Button'
 import InputNft from '@/components/InputNft'
 import Title from '@/components/common/Title/Title'
-import Image from 'next/image'
+import Faucet from '@/components/Faucet/Faucet'
+
+// ABI
+import { contractABI as ABI } from '@/abi/contractABI'
 
 const SectionMint = () => {
+    const { address } = useAccount()
+    const [inputValue, setInputValue] = useState('1')
+    const [quantity, setQuantity] = useState(inputValue)
+    const [hash, setHash] = useState<any>(undefined)
+
+    // prevent excessive request sending
+    const handleSearchDebounced = useRef(
+        debounce((searchText: string) => setQuantity(searchText), 800)
+    ).current
+
+    useEffect(() => {
+        handleSearchDebounced(inputValue)
+    }, [inputValue, handleSearchDebounced])
+
+    const {
+        config,
+        error,
+        isFetching: isPrepareFetching,
+        isLoading: isPrepareLoading,
+    } = usePrepareContractWrite({
+        enabled: true,
+        address: process.env.NEXT_PUBLIC_CONTRACT as `0x${string}`,
+        abi: ABI,
+        functionName: 'claim',
+        args: [
+            address!,
+            parseUnits(quantity, 0),
+            '0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE',
+            parseEther('0'),
+            {
+                proof: [],
+                quantityLimitPerWallet: parseUnits('3', 0),
+                pricePerToken: parseUnits('0', 0),
+                currency: '0x0000000000000000000000000000000000000000',
+            },
+            '0x',
+        ],
+        value: parseEther('0'),
+    })
+
+    const { write, data: cWData } = useContractWrite({
+        ...config,
+        onSuccess(data) {
+            setHash(data?.hash)
+        },
+    })
+
+    const {
+        data: receiptData,
+        isError,
+        isLoading: isReceiptLoading,
+    } = useWaitForTransaction({
+        hash,
+        onSettled() {
+            setHash(undefined)
+        },
+    })
+
+    console.log({ cWData })
+    console.log({ receiptData })
+    console.log(
+        receiptData?.logs?.[0]?.topics?.[3]
+            ? fromHex(receiptData?.logs?.[0]?.topics?.[3], 'number')
+            : 'no data'
+    )
+
+    //read method on blockchain tokenURI
+
     return (
         <section className="pt-[10rem]">
             <Title title="Mint now">
                 Lorem Ipsum is simply dummy text of the printy. Minted 21 of
                 1000.
             </Title>
+
+            <Faucet />
+
             <div className="mx-auto mb-[10rem] flex max-w-[640px] bg-linen">
                 <div className="flex-column flex-row items-center p-10 text-center">
-                    <InputNft />
+                    <InputNft
+                        value={inputValue}
+                        setValue={setInputValue}
+                    />
                     <p className="mb-9 text-sm">
                         You’ve haven’t reached your minting limit.
                     </p>
-                    <Button>MINT</Button>
+                    <Button
+                        type="button"
+                        // disabled
+                        disabled={!write}
+                        onClick={() => write?.()}
+                    >
+                        MINT
+                    </Button>
+                    {(isPrepareFetching || isPrepareLoading) && (
+                        <div>loading...</div>
+                    )}
+                    <div>{quantity}</div>
                 </div>
                 <div className="p-10 text-center">
                     <Image
@@ -54,3 +155,33 @@ const SectionMint = () => {
 }
 
 export default SectionMint
+
+// two tokens minted:
+
+// {address
+// :
+// "0x2b2d8aa76e03fedfd22e9b5e333f9eae10488014"
+// blockHash
+// :
+// "0x09e7926138968f21cc5425d1ed9a8a33c4a9994b5e3c8a8f4c5f4bb438a5062b"
+// blockNumber
+// :
+// 38751829n
+// data
+// :
+// "0x"
+// logIndex
+// :
+// 69
+// removed
+// :
+// false
+// topics
+// :
+// (4) ['0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef', '0x0000000000000000000000000000000000000000000000000000000000000000', '0x00000000000000000000000054c28209aa8893bb7b403d91b24ca48eef262788', '0x0000000000000000000000000000000000000000000000000000000000000012']
+// transactionHash
+// :
+// "0xbca7d998901fa49bc4d5e1a9f7508f8dc182e161dc41629b9ac70b7544db6d78"
+// transactionIndex
+// :
+// 5}
--- components/shared/ButtonConnect/ButtonConnect.tsx
+++ components/shared/ButtonConnect/ButtonConnect.tsx
@@ -1,15 +1,19 @@
 'use client'
-import { useAccount, useConnect } from 'wagmi'
+import { useAccount, useConnect, useNetwork } from 'wagmi'
 
 import Button from '@/components/common/Button'
-import useIsMounted from '@/app/hooks/useIsMounted'
 import { useEffect } from 'react'
 
 const ButtonConnect = () => {
-    const isMounted = useIsMounted()
     const { connector: activeConnector, isConnected } = useAccount()
     const { connect, connectors, error, isLoading, pendingConnector } =
-        useConnect()
+        useConnect({
+            chainId: Number(process.env.NEXT_PUBLIC_CHAIN_ID),
+        })
+
+    const { chain, chains } = useNetwork()
+
+    console.log('CHAIN CONNECTED: ', chain)
 
     useEffect(() => {
         if (error) alert(error.message)
@@ -17,20 +21,22 @@ const ButtonConnect = () => {
 
     return (
         <>
-            {isConnected && <div>Connected to {activeConnector?.id}</div>}
+            {isConnected && <div>Connected to {activeConnector?.name}</div>}
 
             {connectors.map((connector) => (
                 <Button
-                    className="ml-auto"
+                    className="ml-auto flex min-w-[11.5rem] items-center justify-center"
                     type="button"
-                    disabled={isMounted ? !connector.ready : false}
+                    disabled={!connector.ready}
                     key={connector.id}
-                    onClick={() => connect({ connector })}
+                    onClick={() => {
+                        connect({ connector })
+                    }}
                 >
-                    {isMounted ? connector.name : '...'}
+                    {connector.name}
                     {isLoading &&
                         pendingConnector?.id === connector.id &&
-                        ' (connecting)'}
+                        ' ...'}
                 </Button>
             ))}
         </>
