f7e6e7bff99516c72dc934e04c55eca9de26689c
danilo-89 <danilo89abcd@gmail.com>
2023-08-15 00:20:42 +0200

    style changes

--- components/common/Modal/Modal.tsx
+++ components/common/Modal/Modal.tsx
@@ -17,7 +17,7 @@ const Modal = ({ children, preventOverlayClose, setIsOpen }: IProps) => {
             <Dialog.Portal>
                 <Dialog.Overlay className="fixed inset-0 z-10 flex items-center justify-center bg-[rgba(0,0,0,0.75)]">
                     <Dialog.Content
-                        className="mx-auto rounded bg-white"
+                        className="mx-auto max-w-[100%] rounded bg-white"
                         onInteractOutside={(e) => {
                             if (preventOverlayClose) e.preventDefault()
                         }}
