2310000ad833b25cfc183d644aa3ed01cc657d2d
danilo-89 <danilo89abcd@gmail.com>
2023-08-15 00:18:53 +0200

    component added

---
+++ components/common/LoaderDots/LoaderDots.tsx
@@ -0,0 +1,11 @@
+const LoaderDots = () => {
+    return (
+        <span className="inline-block">
+            <span className="animate-[dot-1_1.2s_ease-out_infinite]">.</span>
+            <span className="animate-[dot-2_1.2s_ease-out_infinite]">.</span>
+            <span className="animate-[dot-3_1.2s_ease-out_infinite]">.</span>
+        </span>
+    )
+}
+
+export default LoaderDots
