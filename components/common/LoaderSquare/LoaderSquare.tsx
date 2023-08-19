8e7e95d6831c0e500f83471d126b452b3cc41992
danilo-89 <danilo89abcd@gmail.com>
2023-08-19 18:33:46 +0200

    loader component added

---
+++ components/common/LoaderSquare/LoaderSquare.tsx
@@ -0,0 +1,41 @@
+import { SVGProps } from 'react'
+
+const LoaderSquare = (props: SVGProps<SVGSVGElement>) => (
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width={40}
+        height="auto"
+        viewBox="0 0 100 100"
+        {...props}
+    >
+        <path
+            fill="none"
+            stroke="#dcdcdc"
+            strokeWidth={20}
+            d="M20 20h60v60H20z"
+        />
+        <path
+            fill="none"
+            stroke="#61C1CE"
+            strokeWidth={20}
+            d="M20 20h60v60H20z"
+        >
+            <animate
+                attributeName="stroke-dasharray"
+                dur="1s"
+                keyTimes="0;0.5;1"
+                repeatCount="indefinite"
+                values="24 216;120 120;24 216"
+            />
+            <animate
+                attributeName="stroke-dashoffset"
+                dur="1s"
+                keyTimes="0;0.5;1"
+                repeatCount="indefinite"
+                values="0;-120;-240"
+            />
+        </path>
+    </svg>
+)
+
+export default LoaderSquare
