2e59fd974f84b185a1083e1d4bcf4b4a07eb6b8a
danilo-89 <danilo89abcd@gmail.com>
2023-08-19 18:46:02 +0200

    requests added

---
+++ requests/requests.ts
@@ -0,0 +1,35 @@
+import axios from 'axios'
+
+export const getNFTs = async (address: string | undefined) => {
+    const res = await axios.get(
+        `https://${'polygon-mumbai'}.g.alchemy.com/nft/v2/${
+            process.env.NEXT_PUBLIC_ALCHEMY
+        }/getNFTs`,
+        {
+            params: {
+                owner: address,
+                contractAddresses: [process.env.NEXT_PUBLIC_CONTRACT],
+            },
+        }
+    )
+    return res
+}
+
+export const getNFTMetadata = async (tokenId?: number | undefined) => {
+    if (tokenId) {
+        const res = await axios.get(
+            `https://${'polygon-mumbai'}.g.alchemy.com/nft/v2/${
+                process.env.NEXT_PUBLIC_ALCHEMY
+            }/getNFTMetadata`,
+            {
+                params: {
+                    tokenId: tokenId,
+                    contractAddress: process.env.NEXT_PUBLIC_CONTRACT,
+                    tokenType: 'ERC721',
+                },
+            }
+        )
+        return res
+    }
+    return undefined
+}
