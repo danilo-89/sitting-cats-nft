2004c12f2e2e2b085453411323752514bfeb3d65
danilo-89 <danilo89abcd@gmail.com>
2023-08-20 17:42:10 +0200

    data added

---
+++ components/sections/SectionRoadmap/data.ts
@@ -0,0 +1,20 @@
+export const roadmapPhasesConfig = [
+    {
+        name: 'Phase 1',
+        description: 'Launch and Growth',
+        src: '/rocket.png',
+        alt: 'rocket illustration',
+    },
+    {
+        name: 'Phase 2',
+        description: '',
+        src: '/question-mark.png',
+        alt: 'question mark illustration',
+    },
+    {
+        name: 'Phase 3',
+        description: 'PROFIT!',
+        src: '/coins.png',
+        alt: 'coins illustration',
+    },
+]
