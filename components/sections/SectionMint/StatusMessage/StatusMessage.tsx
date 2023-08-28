753e535114be3d0d461302cdc9d89bd956b07ac6
danilo-89 <danilo89abcd@gmail.com>
2023-08-28 17:18:54 +0200

    total amount added

--- components/sections/SectionMint/StatusMessage/StatusMessage.tsx
+++ components/sections/SectionMint/StatusMessage/StatusMessage.tsx
@@ -1,16 +1,46 @@
+import clsx from 'clsx'
+
+// Hooks
+import useIsWrongNetwork from '@/hooks/useIsWrongNetwork'
+
 interface IProps {
     userPhaseNftBalance: number | undefined
     limitPerWallet: number | undefined
+    totalPrice: number | undefined
+    isEnoughBalanceToMint: boolean
 }
 
-const StatusMessage = ({ userPhaseNftBalance, limitPerWallet }: IProps) => {
+const StatusMessage = ({
+    userPhaseNftBalance,
+    limitPerWallet,
+    totalPrice,
+    isEnoughBalanceToMint,
+}: IProps) => {
+    const { preferredNetwork } = useIsWrongNetwork()
+
     return (
         <>
-            {userPhaseNftBalance} of {limitPerWallet} NFT
-            {typeof userPhaseNftBalance === 'number' && userPhaseNftBalance > 1
-                ? 's'
-                : ''}{' '}
-            claimed
+            {totalPrice ? (
+                <span
+                    className={clsx(
+                        isEnoughBalanceToMint
+                            ? 'bg-antiFlashWhite'
+                            : 'bg-[#ffc5c5]',
+                        'mb-2 block  px-3 py-1.5 font-bold'
+                    )}
+                >
+                    Price: {totalPrice}{' '}
+                    {preferredNetwork?.nativeCurrency?.symbol}
+                </span>
+            ) : null}
+            <span>
+                {userPhaseNftBalance} of {limitPerWallet} NFT
+                {typeof userPhaseNftBalance === 'number' &&
+                userPhaseNftBalance > 1
+                    ? 's'
+                    : ''}{' '}
+                claimed
+            </span>
         </>
     )
 }
