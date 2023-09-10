15cd50180e4c91e44cca9cbabc31f1130379e90b
danilo-89 <danilo89abcd@gmail.com>
2023-09-10 21:06:00 +0200

    meta tags added

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,6 +32,60 @@ export default function RootLayout({
     return (
         <html lang="en">
             <head>
+                {/* <!-- HTML Meta Tags --> */}
+                <title>Sitting Cats NFT</title>
+                <meta
+                    name="description"
+                    content="Demonstrating NFT minting on the Polygon Mumbai test network"
+                />
+
+                {/* <!-- Facebook Meta Tags --> */}
+                <meta
+                    property="og:url"
+                    content="https://sitting-cats-nft.vercel.app/"
+                />
+                <meta
+                    property="og:type"
+                    content="website"
+                />
+                <meta
+                    property="og:title"
+                    content="Sitting Cats NFT"
+                />
+                <meta
+                    property="og:description"
+                    content="Demonstrating NFT minting on the Polygon Mumbai test network"
+                />
+                <meta
+                    property="og:image"
+                    content="https://sitting-cats-nft.vercel.app/og-cover.jpg"
+                />
+
+                {/* <!-- Twitter Meta Tags --> */}
+                <meta
+                    name="twitter:card"
+                    content="summary_large_image"
+                />
+                <meta
+                    property="twitter:domain"
+                    content="sitting-cats-nft.vercel.app"
+                />
+                <meta
+                    property="twitter:url"
+                    content="https://sitting-cats-nft.vercel.app/"
+                />
+                <meta
+                    name="twitter:title"
+                    content="Sitting Cats NFT"
+                />
+                <meta
+                    name="twitter:description"
+                    content="Demonstrating NFT minting on the Polygon Mumbai test network"
+                />
+                <meta
+                    name="twitter:image"
+                    content="https://sitting-cats-nft.vercel.app/og-cover.jpg"
+                />
                 <link
                     rel="apple-touch-icon"
                     sizes="180x180"
