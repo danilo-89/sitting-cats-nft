e59f34db8a018cf4123fd57808bffc53c8757aba
danilo-89 <danilo89abcd@gmail.com>
2023-10-18 12:25:40 +0200

    modified to use rainbowkit

--- components/shared/ButtonConnect/ButtonConnect.tsx
+++ components/shared/ButtonConnect/ButtonConnect.tsx
@@ -2,49 +2,32 @@ import { useConnect } from 'wagmi'
 
 // Components
 import Button from '@/components/common/Button'
+import { useConnectModal } from '@rainbow-me/rainbowkit'
 
 const ButtonConnect = () => {
-    const { connect, connectors, isLoading, pendingConnector } = useConnect({
+    const { openConnectModal } = useConnectModal()
+    const { isLoading } = useConnect({
         chainId: Number(process.env.NEXT_PUBLIC_CHAIN_ID),
     })
 
-    const metamaskAvaiable = typeof window?.ethereum !== 'undefined'
-
-    if (!metamaskAvaiable)
-        return (
-            <Button
-                type="button"
-                className="ml-auto flex min-w-[11.5rem] items-center justify-center"
-                data-cy="btn-connect"
-                onClick={() => {
-                    window.open(
-                        `https://metamask.app.link/dapp/${window.location.host}`
-                    )
-                }}
-            >
-                Connect Wallet
-            </Button>
-        )
-
     return (
         <>
-            {connectors.map((connector) => (
+            {openConnectModal ? (
                 <Button
                     className="ml-auto flex min-w-[11.5rem] items-center justify-center"
                     type="button"
-                    disabled={!connector.ready}
-                    key={connector.id}
+                    // disabled={!connector.ready}
+                    // key={connector.id}
                     onClick={() => {
-                        connect({ connector })
+                        openConnectModal()
+                        // connect({ connector })
                     }}
                     data-cy="btn-connect"
                 >
                     Connect Wallet
-                    {isLoading &&
-                        pendingConnector?.id === connector.id &&
-                        ' ...'}
+                    {isLoading && ' ...'}
                 </Button>
-            ))}
+            ) : null}
         </>
     )
 }
