6c0765623d266b2c2dccfd74d674790347b20c16
dev0x7 <developing2021@gmail.com>
2025-04-29 22:03:49 +0200

    dependencies update

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { WagmiProvider, http } from 'wagmi'
 import { defineChain } from 'viem'
+import { polygonAmoy } from 'viem/chains'
 import { RainbowKitProvider, getDefaultConfig } from '@rainbow-me/rainbowkit'
 import { metaMaskWallet, injectedWallet } from '@rainbow-me/rainbowkit/wallets'
 import '@rainbow-me/rainbowkit/styles.css'
@@ -28,43 +29,7 @@ const WALLET_CONNECT_PROJECT_ID = process.env
     .NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID as string
 const ALCHEMY_ID = process.env.NEXT_PUBLIC_ALCHEMY as string
 
-const preferredChain = defineChain({
-    id: Number(CHAIN_ID),
-    name: 'Polygon Amoy',
-    network: 'polygonamoy',
-    nativeCurrency: {
-        name: 'MATIC',
-        symbol: 'MATIC',
-        decimals: 18,
-    },
-    rpcUrls: {
-        alchemy: {
-            http: ['https://polygon-amoy.g.alchemy.com/v2'],
-            webSocket: ['wss://polygon-amoy.g.alchemy.com/v2'],
-        },
-        infura: {
-            http: ['https://polygon-amoy.infura.io/v3'],
-            webSocket: ['wss://polygon-amoy.infura.io/ws/v3'],
-        },
-        default: {
-            http: [RPC_PUBLIC],
-        },
-        public: {
-            http: [RPC_PUBLIC],
-        },
-    },
-    blockExplorers: {
-        etherscan: {
-            name: 'PolygonScan',
-            url: 'https://amoy.polygonscan.com',
-        },
-        default: {
-            name: 'PolygonScan',
-            url: 'https://amoy.polygonscan.com',
-        },
-    },
-    testnet: true,
-})
+const preferredChain = polygonAmoy
 
 const config = getDefaultConfig({
     appName: 'SittingCatsNFT',
